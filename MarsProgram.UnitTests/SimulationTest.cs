2cb6bf3a395f1bf0576799a935e4dbeddf060a74
Aaron Cooper <snazzie@snazzie.space>
2024-12-08 20:37:08 +0000

    update input to be 1:1 to requirement input

--- MarsProgram.UnitTests/SimulationTest.cs
+++ MarsProgram.UnitTests/SimulationTest.cs
@@ -13,8 +13,10 @@ public class SimulationTest
                                    5  3 
                                    1  1  E 
                                    RFRFRFRF 
+                                   
                                    3  2  N 
                                    FRRFLLFFRRFLL 
+                                   
                                    0  3  W 
                                    LLFFFLFLFL
                                    """;
